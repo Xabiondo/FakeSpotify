a6dd9990fca36890985b9d35d11be950f5a74839
xabiondo <xgarciaber@educacion.navarra.es>
2025-12-18 15:07:09 +0100

    Angular descargado

---
+++ frontEndAngular/src/app/components/buscadorBarra/buscadorBarra.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscador',
+  imports: [],
+  templateUrl: './buscadorBarra.html',
+  styleUrl: './buscadorBarra.css',
+})
+export class Buscador {
+
+}
